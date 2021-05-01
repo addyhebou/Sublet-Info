0ca52c61dd545d9074a72f33b9343256dc0986b2
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2021-05-01 18:15:47 -0400

    feat(body): deadline included with decisions at top

--- app/src/components/Body.js
+++ app/src/components/Body.js
@@ -5,6 +5,9 @@ export default class Header extends Component {
         return (
             <div className = "Body">
                 <p>Thank you so much for your interest in “Addy’s Room”, the summer sublet room that will run from June 7, 2021 - August 31, 2021.</p>
+                <strong>APPLICATION DEADLINE: MAY 5</strong>
+                <br/>
+                <strong>Decisions by: MAY 7</strong>
                 <p><strong>Sublet Application Process</strong></p>
                 <p>To give you insight, this is how the entire process will go:</p>
                 <Steps/>
