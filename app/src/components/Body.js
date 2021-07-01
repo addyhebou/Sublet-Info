c1b39983a81ee0ce0befff37fa58ba706d27ef17
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2021-07-01 11:56:55 -0400

    feat(content): updated for august 2021

--- app/src/components/Body.js
+++ app/src/components/Body.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className = "Body">
-                <p>Thank you so much for your interest in “Addy’s Room”, the summer sublet room that will run from August 10, 2021 - August 31, 2021 (21 days).</p>
+                <p>Thank you so much for your interest in “Addy’s Room”, the summer sublet room that will run from August 11, 2021 - August 31, 2021 (20 days).</p>
                 <strong>APPLICATION DEADLINE: July 15</strong>
                 <br/>
                 <strong>Decisions by: July 17</strong>
